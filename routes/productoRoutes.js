132e65e230a179d29a529c7d4cfcf38e39dfe7c2
MarioYiCarrion <147116987+MarioYiCarrion@users.noreply.github.com>
2025-04-19 02:07:42 -0500

    Update productoRoutes.js

--- routes/productoRoutes.js
+++ routes/productoRoutes.js
@@ -14,4 +14,18 @@ router.put('/:id', productoController.updateproducto);
 // Eliminar producto
 router.delete('/:id', productoController.deleteproducto);
 
+// obtener productos por tipo
+app.get('/api/productos/tipo/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+      const [rows] = await connection.query(
+        'SELECT * FROM producto WHERE id_tipoproducto = ?',
+        [id]
+      );
+      res.json(rows);
+    } catch (error) {
+      res.status(500).json({ error: 'Error al obtener productos' });
+    }
+  });
+
 module.exports = router;
